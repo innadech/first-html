8fe838bced4d0a115be16760e8f4d7a988143268
YouROCK <yourock88@meta.ua>
2025-01-07 18:56:23 +0200

    for in

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log('hello Inna!')
+
+const obj = {
+  'foo': '1',
+  'bar': '2',
+  'baz': '3'
+}
+
+for (const key in obj) {
+  console.log(key)
+  console.log(obj[key])
+  console.log(obj.baz);
+}
